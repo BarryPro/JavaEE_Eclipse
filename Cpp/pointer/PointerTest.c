83beea6bbdfcdfe61b5e99e9f5e3642c5b4ca66d
belong <belong.belong@outlook.com>
2017-08-23 21:03:04 +0800

    添加python工程，eclipse是一个统一的IDE

--- Cpp/pointer/PointerTest.c
+++ Cpp/pointer/PointerTest.c
@@ -4,11 +4,14 @@
  * 指针测试
  */
 int main(void){
-	char* str = "ASFDJKFD";
+	char* str = "ABCDEFGHIJK";
 	int* pt = (int*)str;
-	printf("%c\n%c\n",*(str+1),(char*)(pt+1));
+	printf("%c\n%c\n",*(str+1),(char*)(pt+2));
 	printf(str);
 	printf("\n");
 	printf(pt);
+	printf("\n测试指针类型的大小\n");
+	printf("char*:%d\n",sizeof(str));
+	printf("int*:%d\n",sizeof(pt));
 	return 0;
 }
